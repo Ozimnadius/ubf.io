9e3c635e08b651033f609705510322f54d8ad941
Seregin Mikle <mikhail-seregin@yandex.ru>
2019-04-02 11:38:48 +0300

    end

---
+++ smartgrid.js
@@ -0,0 +1,35 @@
+const smartgrid = require('smart-grid');
+
+smartgrid('./css/layout', {
+    mobileFirst: false,
+    columns: 24,
+    offset: "20px",
+    outputStyle: "scss",
+    container: {
+        maxWidth: "975px",
+        fields: "30px",
+        containerWidth: "975px"
+    },
+    breakPoints: {
+        lg: {
+            width: "1199.99px",
+            fields: "15px",
+            containerWidth: "975px"
+        },
+        md: {
+            width: "991.99px",
+            fields: "15px",
+            containerWidth: "750px"
+        },
+        sm: {
+            width: "767.99px",
+            fields: "15px",
+            containerWidth: "570px"
+        },
+        xs: {
+            width: "575.99px",
+            fields: "15px",
+            containerWidth: "100%"
+        }
+    },
+});
